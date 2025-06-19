24885633d35f977c41b3eb270de4d1e19e1ff6c4
yakov tfilin <205momi@gmail.com>
2025-06-19 14:21:19 +0300

    Add magen.py

---
+++ magen.py
@@ -0,0 +1,40 @@
+#x = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34]
+#y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#count = 0
+#for i in range(len(x) - 2):
+   # if x[i] + x[i + 2] == x[i + 1] * 2:
+        #y[count] = x[i]
+       # count += 1
+#print(y)
+
+#list_a, list_b, list_c = [], [], []
+
+#for num in range(4):
+   # list_a.append(num * 2)
+#print(list_a)
+#mid = len(list_a) // 2
+#max_val = max(list_a)
+#info = {"mid": mid, "max_val": max_val}
+#for i in range(info["max_val"]):
+    #if i < len(list_a):
+       # item = list_a[i]
+       # list_b.append(item + 1)
+    #else:
+       # list_b.append(i % 3)
+#print(list_b)
+#while len(list_b) > 1:
+    #x = list_b.pop(0) * list_b.pop(0)
+    #list_c.append(x)
+#print(list_c)
+#is_max_in_list = info['max_val'] in list_c
+#print(is_max_in_list)
+#print(is_max_in_list or bool(len(list_a)))
+
+#x = 5
+#for i in range(x):
+#s = ""
+   # for j in range(x - i):
+       # s += str(x - i - j)
+   # print(s)
+
+#'] in list_3)
